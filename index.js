43ffa8cee77b515a3ea258e1468b29ea50088b48
Guru EC2 SSH Key <agurusankar@gmail.com>
2019-01-13 19:59:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //importing node framework
-var express = require(‘express’);
+const express = require(‘express’);
  
-var app = express();
+const app = express();
+const PORT = 3000;
 //Respond with "hello world" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
  res.send(‘hello world’);
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () =>
+          console.log("Server running ")
+);
  
 module.exports = app;
